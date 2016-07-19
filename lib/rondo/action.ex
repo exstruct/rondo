64748dbca46cddfa64706405c227ca201f105d9b
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-07-19 01:52:18 -0600

    implement action with events

--- lib/rondo/action.ex
+++ lib/rondo/action.ex
@@ -6,15 +6,15 @@ defmodule Rondo.Action do
 
   defmacro __using__(_) do
     quote do
-      import unquote(__MODULE__), only: [action: 2, action: 3]
+      import unquote(__MODULE__), only: [action: 2, action: 3, action: 4]
     end
   end
 
-  def action(reference, handler, props \\ %{})
-  def action(reference = %Rondo.Store.Reference{}, handler, props) do
-    %__MODULE__{reference: reference, handler: handler, props: props}
+  def action(reference, handler, props \\ %{}, events \\ [])
+  def action(reference = %Rondo.Store.Reference{}, handler, props, events) do
+    %__MODULE__{reference: reference, handler: handler, props: props, events: events}
   end
-  def action(nil, handler, props) do
-    %__MODULE__{handler: handler, props: props}
+  def action(nil, handler, props, events) do
+    %__MODULE__{handler: handler, props: props, events: events}
   end
 end
