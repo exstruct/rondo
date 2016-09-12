741acb2c807e301db7edac35d22019539236ae9d
Cameron Bytheway <bytheway.cameron@gmail.com>
2016-09-12 15:15:13 -0600

    release 0.1.5

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Rondo.Mixfile do
 
   def project do
     [app: :rondo,
-     version: "0.1.4",
+     version: "0.1.5",
      elixir: "~> 1.1",
      description: "component rendering library",
      test_coverage: [tool: ExCoveralls],
